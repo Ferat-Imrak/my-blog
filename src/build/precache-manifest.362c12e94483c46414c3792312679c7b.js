faf9c56a21952880ee35874627b50108544c9b30
Ferhat <ferhat@MacBook-Pro.local>
2020-08-19 22:30:49 -0500

    first commit

---
+++ src/build/precache-manifest.362c12e94483c46414c3792312679c7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f751286a4ef61d1cb685e6500a96b6e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1a0a4deb928a990c3b2",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "c111e6ab5ad091c19bc7",
+    "url": "/static/js/2.b1ace6ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b1ace6ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a0a4deb928a990c3b2",
+    "url": "/static/js/main.8670a33e.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
